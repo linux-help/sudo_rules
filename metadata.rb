cf47bb02eb41d0aa7bd90a57e22896b515727c7c
Eric Renfro <psi-jack@linux-help.org>
2015-05-13 14:04:38 -0400

    Update metadata to include license

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'sudo_rules'
 maintainer       'Linux-Help.org'
 maintainer_email 'erenfro@linux-help.org'
-license          'All rights reserved'
+license          'GNU Lesser General Public License v3'
 description      'Configures sudo rules from data bags using the sudo cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 %w(redhat centos fedora ubuntu debian freebsd mac_os_x).each do |os|
       supports os
