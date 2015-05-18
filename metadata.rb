ebf79edf8d648ca5f59c25efb76ad813b705ad19
Eric Renfro <psi-jack@linux-help.org>
2015-05-18 10:42:54 -0400

    New feature added, version bump.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'erenfro@linux-help.org'
 license          'GNU Lesser General Public License v3'
 description      'Configures sudo rules from data bags using the sudo cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 %w(redhat centos fedora ubuntu debian freebsd mac_os_x).each do |os|
       supports os
